3f1f06a5fd7eecfc9e4fb513cb67ab00b4b6f4b0
Frank Bentrem <sciguy137@gmail.com>
2017-03-14 21:16:54 -0400

    Change app name. Updated requirements.

--- app/settings.py
+++ app/settings.py
@@ -5,7 +5,7 @@
 import os
 
 # Application settings
-APP_NAME = "Flask-User starter app"
+APP_NAME = "Prediction Tycoon"
 APP_SYSTEM_ERROR_SUBJECT_LINE = APP_NAME + " system error"
 
 # Flask settings
